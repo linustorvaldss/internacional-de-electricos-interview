09aaa50c411ff709016f45021b2998f0862946ce
linustorvaldss <182672946+linustorvaldss@users.noreply.github.com>
2026-03-28 12:03:55 -0500

    add swagger api documentation

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Internacional de Electricos API')
+    .setDescription('Prueba técnica')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
