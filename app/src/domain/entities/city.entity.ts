468b6542dbc9a8edbd2def25cc2b026357afbecd
linustorvaldss <182672946+linustorvaldss@users.noreply.github.com>
2026-03-28 13:03:08 -0500

    add build stage Dockerfile

--- app/src/domain/entities/city.entity.ts
+++
@@ -1,9 +0,0 @@
-export class CityEntity {
-  constructor(
-    public id: number,
-    public name: string,
-    public department_id: number,
-    public created_at: Date,
-    public updated_at: Date,
-  ) {}
-}
--- app/src/domain/entities/department.entity.ts
+++
@@ -1,8 +0,0 @@
-export class DepartmentEntity {
-  constructor(
-    public id: number,
-    public name: string,
-    public created_at: Date,
-    public updated_at: Date,
-  ) {}
-}
