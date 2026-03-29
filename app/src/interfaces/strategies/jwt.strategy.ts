8e59e5685f50a3d942c180d3e53ff021e8ec4287
linustorvaldss <182672946+linustorvaldss@users.noreply.github.com>
2026-03-28 19:56:06 -0500

    validate tokens payload with passport

---
+++ app/src/interfaces/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserResponse } from '../dtos/users/response-user.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SEED || 'fallback-secret',
+    });
+  }
+
+  async validate(payload: any): Promise<UserResponse> {
+    return {
+      id: payload.id,
+      name: payload.name,
+      email: payload.email,
+      status: payload.status,
+      created_at: payload.created_at,
+      updated_at: payload.updated_at,
+    };
+  }
+}
